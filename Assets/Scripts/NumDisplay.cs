5bb106127cf13e19e314bc2ce52ea3146240fc5d
IwamotoKakeru <kganpipi@gmail.com>
2023-06-26 05:27:14 +0900

    First Commit

---
+++ Assets/Scripts/NumDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private TextMesh textMesh;
+    private int num;
+    void Start()
+    {
+        textMesh = GetComponent<TextMesh>();
+    }
+
+    public void DisplayNum(int numGetter){
+        num = numGetter;
+    }
+
+    void Update(){
+        textMesh.text = num.ToString("0");
+    }
+
+}
